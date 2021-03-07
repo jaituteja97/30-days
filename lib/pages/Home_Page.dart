8b07ae16aee91cf33b752497f17fc16ba2ec407f
Jai Tuteja <jaituteja90@gmail.com>
2021-03-07 13:45:25 +0530

    day 3

---
+++ lib/pages/Home_Page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  static const String homeRoute = "/";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("30 days of flutter")));
+  }
+}
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  static const String login = "/loginPage";
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
