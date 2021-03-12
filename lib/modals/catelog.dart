6d5c3225c36d12426e52cef35633871b907f77a8
Jai Tuteja <jaituteja90@gmail.com>
2021-03-12 18:12:01 +0530

    item

---
+++ lib/modals/catelog.dart
@@ -0,0 +1,25 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.imageUrl});
+}
+
+final products = [
+  Item(
+  id: "001",
+  name: "iPhone 12 Pro",
+  desc: "Apple iPhone 12th generation",
+  price: 999,
+  color: '#33505a',
+  imageUrl: "https://cdn.vox-cdn.com/thumbor/T49kxT-ZhzKjyWWWkVYiOIyAOv4=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/11249379/jbareham_171101_2099_A_0088_02.jpg",
+),
+
+  
+
+
+];
